531ef8f64e7e65b97849ecb3a819bea9908d53cf
Siwawes Wongcharoen <siwaoh@gmail.com>
2016-10-31 23:23:05 +0700

    add fragment state save-restore

--- app/src/main/java/com/puuga/testbottomnavigationview/AFragment.java
+++ app/src/main/java/com/puuga/testbottomnavigationview/AFragment.java
@@ -2,12 +2,17 @@ package com.puuga.testbottomnavigationview;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class AFragment extends Fragment {
+
+    private static final String COUNT = "COUNT";
+    private int count = 0;
 
     public AFragment() {
         // Required empty public constructor
@@ -29,4 +34,18 @@ public class AFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_a, container, false);
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(COUNT, count);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(COUNT)+1;
+            Log.d("a_count", "count: " + count);
+        }
+    }
 }
